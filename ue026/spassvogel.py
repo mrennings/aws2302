4ee287e9420b35c705948b9c16d45db26ff225f1
Markus Rennings <markus@rennings.net>
2023-03-16 13:03:12 +0100

    Übung 026: Spassvogel

---
+++ ue026/spassvogel.py
@@ -0,0 +1,23 @@
+from random import choice
+
+witze = [
+    "Hast du ein Bad genommen?“ – Warum, fehlt eins?",
+    "Egal, wie gut du schläfst: Albert schläft wie Einstein",
+    "Wissenschaftler haben herausgefunden – und sind wieder reingegangen.",
+    "Sitzt einer im Stehcafé.",
+    "Wie heißt der Schutzpatron der Vergesslichen? – Dings",
+    "Hast du was zu trinken? – Wasser. – Nee, was Härteres! – Ok, Eis.",
+    "Was ist rot und steht im Wald? – Ein Kirsch",
+    "Was ist weiß und rollt den Berg rauf? – Eine Lawine mit Heimweh",
+    "Wie nennt man einen Bumerang, der nicht zurückkommt? – Stock",
+    "Was ist das Gegenteil von Reformhaus? – Reh hinterm Haus",
+    "Wie nennt man ein Delfin in Unterhose? – Slipper",
+    "Egal wie voll du bist, Rudi war Völler."
+]
+
+weiter = True
+while (weiter):
+    print(choice(witze))
+    wahl = input("Noch ein Witz? JA/NEIN ")
+    if (wahl.lower() != "ja"):
+        weiter = False
