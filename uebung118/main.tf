d2ea51a9ab38eddc46f68795750f94759781e113
Markus Rennings <markus@rennings.net>
2023-10-18 13:59:58 +0200

    Optionale Übung 118

---
+++ uebung118/main.tf
@@ -0,0 +1,6 @@
+resource "github_repository" "ue118" {
+  name        = "ue118"
+  description = "Optionale HA"
+  visibility  = "public"
+  auto_init   = true
+}
---
+++ uebung118/variables.tf
@@ -0,0 +1,4 @@
+variable "GITHUB_TOKEN" {
+  # $ export TF_VAR_GITHUB_TOKEN=$(gh auth token)
+  type = string
+}
